e122a76ac3c40aa1539804470dab4b8be72bd46d
HediK <hedi0475@gmail.com>
2019-03-21 18:13:29 +0100

    implemented bottom nav

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import styled from 'styled-components'
-import { Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Feather';
 import Home from './src/pages/Home'
+import WebBrowser from './src/pages/WebBrowser'
+import About from './src/pages/About'
 
-const AppContainer = styled.View`
-  background-color: #090909;
-  flex: 1;
-`
+const TabNavigator = createBottomTabNavigator({
+  Downloader: Home,
+  Pornhub: WebBrowser,
+  About: About,
+},   {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Icon;
+        let iconName;
+        if (routeName === 'Downloader') {
+          iconName = `download`;
+        } else if (routeName === 'Pornhub') {
+          iconName = `film`;
+        } else if (routeName === 'About') {
+          iconName = `info`;
+        }
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <AppContainer>
-        <Home />
-      </AppContainer>
-    );
+        return <IconComponent name={iconName} size={20} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#ff9900',
+      // activeBackgroundColor: '#161414',
+      inactiveTintColor: '#3b3a3a',
+      style: {
+        backgroundColor: '#000000',
+        height: 65,
+      },
+      tabStyle: {
+        padding: 10,
+      },
+      labelStyle: {
+        fontSize: 13,
+        margin: 0
+      },
+    }
   }
-}
+)
+
+export default createAppContainer(TabNavigator);
