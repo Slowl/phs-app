051d49166537b10ed8126c2a798d61f4f7454c30
HediK <hedi0475@gmail.com>
2019-03-21 13:59:16 +0100

    added function to ask permission and download file

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, PermissionsAndroid, ToastAndroid } from 'react-native'
 import styled from 'styled-components'
+
+import RNFetchBlob from 'rn-fetch-blob'
+const { config, fs } = RNFetchBlob
 
 const ButtonsContainer = styled.View`
   width: 100%;
@@ -26,15 +29,78 @@ const CustomText = styled.Text`
 const Buttons = ({ url, quality }) => {
 
 const donwloadFile = async () => {
-  const id = Math.random().toString(36).slice(8).padEnd(5,0)
-  
+  try {
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+    {
+      title: 'PHS Internal Storage Permission',
+      message:
+        'PHS needs access to your Internal Storage ' +
+        'to download your videos.',
+        buttonPositive: 'OK',
+    },
+  );
+  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    const DownloadDir = fs.dirs.DownloadDir
+    const id = Math.random().toString(36).slice(8).padEnd(5,0)
+    const options = {
+      fileCache: true,
+      addAndroidDownloads : {
+        useDownloadManager : true,
+        notification : true,
+        path:`${DownloadDir}/PHS/PHS_${quality}p-${id}.mp4`,
+        description : 'Downloading Video.'
+      }
+    }
+
+    ToastAndroid.showWithGravityAndOffset(
+      'Downloading ... ',
+      ToastAndroid.LONG,
+      ToastAndroid.BOTTOM,
+      0,
+      50,
+    );
+
+    config(options).fetch('GET', `${url}`)
+      .then((res) => {
+        ToastAndroid.showWithGravityAndOffset(
+          'Download completed ',
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          0,
+          50,
+        );
+      })
+      .catch(error => {
+        ToastAndroid.showWithGravityAndOffset(
+          'An error occured ...',
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          0,
+          50,
+        );
+        console.error(error);
+      });
+
+    } else {
+      ToastAndroid.showWithGravityAndOffset(
+        'Permission denied, can\'t start the download ...',
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        0,
+        50,
+      );
+    }
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
   return (
         <CustomButton
           activeOpacity={.7}
           onPress={donwloadFile}
-          accessibilityLabel="quality button"
+          accessibilityLabel="download button"
         >
           <CustomText>{`${quality}p`}</CustomText>
         </CustomButton>
