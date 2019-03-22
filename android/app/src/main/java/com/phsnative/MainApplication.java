62454111d5bdbaebba39645a344ef9a56fd57c15
HediK <hedi0475@gmail.com>
2019-03-22 16:34:39 +0100

    added webview and progress bar libs

--- android/app/src/main/java/com/phsnative/MainApplication.java
+++ android/app/src/main/java/com/phsnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.phsnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCWebViewPackage(),
             new RNGestureHandlerPackage(),
             new RNFetchBlobPackage(),
             new VectorIconsPackage()
